dc85254b0bac2f0f66c432a8ebf8db96502332e6
Juliano Vaz <julianoctvaz@gmail.com>
2024-10-02 16:37:28 -0300

    Initial Commit

---
+++ Shimmer/ShimmerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShimmerApp.swift
+//  Shimmer
+//
+//  Created by Juliano on 02/10/24.
+//
+
+import SwiftUI
+
+@main
+struct ShimmerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
